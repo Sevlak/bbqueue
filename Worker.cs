3aa9d7addd6af5a5dc7863ffa1e39b1f072dba03
William Alves <willoandrade29@gmail.com>
2024-08-20 22:03:23 -0300

    adds consuming and database update logic
    Signed-off-by: William Alves <willoandrade29@gmail.com>

--- Worker.cs
+++ Worker.cs
@@ -1,15 +1,17 @@
-﻿using RabbitMQ.Client;
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 namespace BBQueue;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
     private readonly IModel _channel;
     private EventingBasicConsumer _consumer;
+    private readonly IServiceProvider _serviceProvider;
 
-    public Worker(ILogger<Worker> logger, IConfiguration configuration)
+    public Worker(IServiceProvider provider, IConfiguration configuration)
     {
         var uri = configuration.GetConnectionString("Queue");
         var factory = new ConnectionFactory{Uri = new Uri(uri)};
@@ -17,8 +19,7 @@ public class Worker : BackgroundService
         _channel = connection.CreateModel();
 
         _channel.QueueDeclare("votes_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
-
-        _logger = logger;
+        _serviceProvider = provider;
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -26,11 +27,21 @@ public class Worker : BackgroundService
         //We don't need a loop to consume votes here since they'll trigger the Received event.
         _consumer = new EventingBasicConsumer(_channel);
 
-        _consumer.Received += (channel, ea) =>
+        _consumer.Received += async (channel, ea) =>
         {
             var body = ea.Body.ToArray();
+            var vote = Encoding.UTF8.GetString(body);
 
-            //do something here
+            if (!Int32.TryParse(vote, out var id))
+            {
+                return;
+            }
+
+            using var scope = _serviceProvider.CreateScope();
+            var ctx = scope.ServiceProvider.GetRequiredService<VotesContext>();
+
+            await ctx.Contestants.Where(c => c.Id == id)
+                .ExecuteUpdateAsync(b => b.SetProperty(x => x.VoteQty, x => x.VoteQty + 1), stoppingToken);
 
             _channel.BasicAck(ea.DeliveryTag, false);
         };
